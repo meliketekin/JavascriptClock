827bf4b57474c5e51b3325a35fbc9a595c0d47af
Melike <meliketekin2243@gmail.com>
2022-01-26 02:04:03 +0300

    kodluyoruzClock

---
+++ README.md
@@ -0,0 +1,27 @@
+# Kodluyoruz Clock
+Bu repo [Kodluyoruz](https://www.kodluyoruz.org/) Front-End eğitiminde oluşturduğum ikinci repo. 
+
+İçerisinde bir adet README dosyası, bir adet index.html, bir adet clock.js, bir adet style.css barındırıyor.
+
+## Installation
+Öncelikle projeyi clonelayın. [Repo-link](https://github.com/meliketekin/kodluyoruzClock)
+
+
+```
+git clone https://github.com/meliketekin/kodluyoruzClock
+```
+
+## Usage
+Projeyi cloneladıktan sonra Visual Studio Code programını açınız.
+Linux için:
+```
+cd kodluyoruzClock
+code .
+```
+## Contributing
+Pull requestler kabul edilir. Büyük değişiklikler için, lütfen önce neyi değiştirmek istediğinizi tartışmak için bir konu açınız.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ clock.js
@@ -0,0 +1,36 @@
+function showTime() {
+    const today = new Date()
+    let day = today.getDay()
+    if(day===1){
+        day="Pazartesi"
+    } else if(day===2) {
+        day="Salı"
+    } else if(day===3) {
+        day="Çarşamba"
+    } else if(day===4) {
+        day="Perşembe"
+    } else if(day===5) {
+        day="Cuma"
+    } else if(day===6) {
+        day="Cumartesi"
+    } else if(day===0) {
+        day="Pazar"
+    }
+    let hour = today.getHours()
+    let minute = today.getMinutes()
+    let second = today.getSeconds()
+
+    if(hour>=12) hour-=12
+    hour = (hour<10) ? "0"+hour : hour
+    minute = (minute<10) ? "0"+minute : minute
+    second = (second<10) ? "0"+second : second
+
+    var clock = document.getElementById("myClock")
+    clock.innerText = `${hour}:${minute}:${second}  ${day}`
+    setTimeout(startTime, 1000);
+}
+
+var myName = document.getElementById("myName")
+myName.innerText = prompt("Lütfen isminizi girin:")
+
+showTime()
